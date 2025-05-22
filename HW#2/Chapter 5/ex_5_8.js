10fe4c1433c6a491749e3ad2a580a29c25d8c74d
GU3611 <kuyd2233@cau.ac.kr>
2025-05-22 10:28:45 +0900

    예제 5-8
    콜백 함수와 클로저(3)

---
+++ HW#2/Chapter 5/ex_5_8.js
@@ -0,0 +1,6 @@
+fruits.forEach(function (fruit) {
+    var $li = document.createElement('li');
+    $li.innerText = fruit;
+    $li.addEventListener('click', alertFruit.bind(null, fruit));
+    $ul.appendChild($li);
+});
